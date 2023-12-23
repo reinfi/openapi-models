f008a1289a82b07fcab0ece231b38a3528b51f9b
Reinfi <reinfandt@z-em.de>
2023-12-23 20:18:13 +0100

    add easy coding standard definition

---
+++ ecs.php
@@ -0,0 +1,32 @@
+<?php
+
+declare(strict_types=1);
+
+use PHP_CodeSniffer\Standards\PSR12\Sniffs\Files\ImportStatementSniff;
+use PhpCsFixer\Fixer\Import\FullyQualifiedStrictTypesFixer;
+use PhpCsFixer\Fixer\Whitespace\TypeDeclarationSpacesFixer;
+use Symplify\CodingStandard\Fixer\LineLength\LineLengthFixer;
+use Symplify\EasyCodingStandard\Config\ECSConfig;
+use Symplify\EasyCodingStandard\ValueObject\Set\SetList;
+
+return static function (ECSConfig $configurator): void {
+    $configurator->parallel();
+
+    $configurator->paths([__DIR__ . '/config/', __DIR__ . '/src/', __DIR__ . '/test/', __DIR__ . '/ecs.php']);
+
+    // import SetList here in the end of ecs. is on purpose
+    // to avoid overridden by existing Skip Option in current config
+    $configurator->import(SetList::PSR_12);
+    $configurator->import(SetList::COMMON);
+    $configurator->import(SetList::NAMESPACES);
+    $configurator->import(SetList::CLEAN_CODE);
+
+    $configurator->ruleWithConfiguration(TypeDeclarationSpacesFixer::class, [
+        'elements' => ['function', 'property'],
+    ]);
+    $configurator->rule(FullyQualifiedStrictTypesFixer::class);
+    $configurator->rule(ImportStatementSniff::class);
+    $configurator->rule(LineLengthFixer::class);
+
+    $configurator->lineEnding("\n");
+};
