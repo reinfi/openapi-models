e838c2f1597d31943b578eaf568bd28d200ab094
Reinfi <reinfandt@z-em.de>
2023-12-23 19:48:22 +0100

    add configuration

---
+++ src/Configuration/Configuration.php
@@ -0,0 +1,16 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Reinfi\OpenApiModels\Configuration;
+
+readonly class Configuration
+{
+    public function __construct(
+        /** @var string[] $paths */
+        public array $paths,
+        public string $outputPath,
+        public string $namespace,
+    ) {
+    }
+}
---
+++ src/Configuration/ConfigurationBuilder.php
@@ -0,0 +1,54 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Reinfi\OpenApiModels\Configuration;
+
+use InvalidArgumentException;
+
+class ConfigurationBuilder
+{
+    public function buildFromFile(string $configurationFile): Configuration
+    {
+        if (! is_file($configurationFile)) {
+            throw new InvalidArgumentException(sprintf('Configuration file %s does not exist', $configurationFile));
+        }
+
+        $configurationValues = require $configurationFile;
+
+        if ($configurationValues instanceof Configuration) {
+            $this->validate($configurationValues);
+
+            return $configurationValues;
+        }
+
+        if (! is_array($configurationValues)) {
+            throw new InvalidArgumentException(
+                sprintf('Configuration file "%s" does not return an array', $configurationFile)
+            );
+        }
+
+        $configuration = new Configuration(
+            $configurationValues['paths'] ?? [],
+            $configurationValues['outputPath'] ?? '',
+            $configurationValues['namespace'] ?? '',
+        );
+
+        $this->validate($configuration);
+
+        return $configuration;
+    }
+
+    private function validate(Configuration $configuration): void
+    {
+        if (count($configuration->paths) === 0) {
+            throw new InvalidArgumentException('There are no paths defined in your configuration');
+        }
+
+        if (! is_dir($configuration->outputPath)) {
+            throw new InvalidArgumentException(
+                sprintf('Output path "%s" is not a directory', $configuration->outputPath)
+            );
+        }
+    }
+}
